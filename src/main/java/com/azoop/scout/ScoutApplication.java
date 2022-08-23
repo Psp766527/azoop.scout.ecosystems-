b7cdfcac21922c374da5788401458f1817b0271a
psp766527 <kushwahpradeep531@gmail.com>
2022-08-24 02:01:12 +0530

    the comments to the ScoutApplication class .

--- src/main/java/com/azoop/scout/ScoutApplication.java
+++ src/main/java/com/azoop/scout/ScoutApplication.java
@@ -3,11 +3,19 @@ package com.azoop.scout;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This is the base application class which is entry point of the Application
+ */
 @SpringBootApplication
 public class ScoutApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScoutApplication.class, args);
-	}
+    /**
+     * This method will we be helping the application to run.
+     *
+     * @param args the arguments array.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ScoutApplication.class, args);
+    }
 
 }
