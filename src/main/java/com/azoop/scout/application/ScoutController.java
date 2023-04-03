832567f1b4dd727e119682a4aa33fc820e6cdb65
GuniorG-Java-Dev-766527 <55358571+Psp766527@users.noreply.github.com>
2023-04-03 23:44:51 +0530

    added comments on ScoutController class

--- src/main/java/com/azoop/scout/application/ScoutController.java
+++ src/main/java/com/azoop/scout/application/ScoutController.java
@@ -2,6 +2,12 @@ package com.azoop.scout.application;
 
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * This controller class will be used to expose some end points which will be used to authenticate the user &
+ * authorize the user for specified resource.
+ */
 @RestController
 public class ScoutController {
+
+
 }
